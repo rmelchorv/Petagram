2eeea63044bb7b0ae84b1b1f29b7b9c1a77763f3
rmelchorv <rmelchorv@gmail.com>
2022-05-24 23:59:19 -0500

    Initial commit!

---
+++ app/src/main/java/mx/edu/unistmo/repo/mobile/android/petagram/MainActivity.java
@@ -0,0 +1,14 @@
+package mx.edu.unistmo.repo.mobile.android.petagram;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
